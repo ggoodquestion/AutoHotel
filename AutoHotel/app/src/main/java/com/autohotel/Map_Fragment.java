342254602496d93a024b1909e5e3b9c0f446ab3e
ggoodquestion <boss.su2000@gmail.com>
2018-11-20 00:38:09 +0800

    123

---
+++ AutoHotel/app/src/main/java/com/autohotel/Map_Fragment.java
@@ -0,0 +1,17 @@
+package com.autohotel;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Map_Fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_map, container, false);
+        return view;
+    }
+}
